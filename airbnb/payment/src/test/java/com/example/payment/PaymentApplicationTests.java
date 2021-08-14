42cfe1c17418f09e6a919b02b8b258a1731b9caf
InHotChicken <gkrgus0810@gmail.com>
2021-08-15 02:56:46 +0900

    feat:payment생성

--- airbnb/payment/src/test/java/com/example/payment/PaymentApplicationTests.java
+++ airbnb/payment/src/test/java/com/example/payment/PaymentApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.review;
+package com.example.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReviewApplicationTests {
+class PaymentApplicationTests {
 
     @Test
     void contextLoads() {
