fe1205cf2a980bcfb1b94ad28599191ed3cd94cf
JHS <russwe719@gmail.com>
2021-08-15 21:04:01 +0900

    feat: kafka stream구성
    1) jackson라이브러리를 통한 자바객체 <-> json
    2) room에 대한 binding 설정(input, output)
    3) binding은 localhost:9093
    4) 방삭제시 리뷰삭제하도록 하는 이벤트 생성(pub)
    5) 자바 객체 -> Json 클래스생성

--- airbnb/room/src/main/java/com/example/room/config/kafka/RoomBinder.java
+++ airbnb/room/src/main/java/com/example/room/config/kafka/RoomBinder.java
@@ -4,14 +4,18 @@ import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface RoomBinder {
     String INPUT = "room-event-in";
     String OUTPUT = "room-event-out";
 
+    // binder에 연결할 binding설정(input = room-event-in)
     @Input(INPUT)
     SubscribableChannel input();
 
+    // binder에 연결할 binding설정(output = room-event-out)
     @Output(OUTPUT)
     MessageChannel output();
 
