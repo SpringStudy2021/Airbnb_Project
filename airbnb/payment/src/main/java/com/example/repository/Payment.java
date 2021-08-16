f2475b8ed672667a4bda07c0ad5df7037d07a866
InHotChicken <gkrgus0810@gmail.com>
2021-08-17 00:23:14 +0900

    feat:PaymentService에 paymentApproved메서드구, H2 DB생성

---
+++ airbnb/payment/src/main/java/com/example/repository/Payment.java
@@ -0,0 +1,10 @@
+package com.example.payment;
+
+public class Payment {
+
+    private Long payId;
+    private Long roomId;
+    private Long rsvID;
+    private Long price;
+    private String status;
+}
