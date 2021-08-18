50dacf0b527e12f38bfb84a8db1773831dfc00cd
cindia3704 <cindia3704@gmail.com>
2021-08-19 02:00:44 +0900

    feat: Review host,customer,room 생성 구현

--- airbnb/review/src/main/java/com/example/review/aggregate/ReviewService.java
+++ airbnb/review/src/main/java/com/example/review/aggregate/ReviewService.java
@@ -1,8 +1,43 @@
 package com.example.review.aggregate;
 
+import com.example.review.model.CustomerReviewCreated;
+import com.example.review.model.HostReviewCreated;
+import com.example.review.model.Review;
+import com.example.review.model.RoomReviewCreated;
+import com.example.review.repository.ReviewRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
 
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public String createRoomReview(RoomReviewCreated roomReviewCreated) {
+        // TODO: 예외처리 (
+        System.out.println("hi");
+        System.out.println("by:"+roomReviewCreated.getCommentBy());
+
+        Review review = Review.of(roomReviewCreated.getCommentBy(),roomReviewCreated.getCommentTo(),roomReviewCreated.getDescription(),roomReviewCreated.getScore());
+        reviewRepository.save(review);
+        return "RoomReview Created";
+    }
+
+    public String createHostReview(HostReviewCreated hostReviewCreated) {
+        // TODO: 예외처리
+        Review review = Review.of(hostReviewCreated.getCommentBy(),hostReviewCreated.getCommentTo(),hostReviewCreated.getDescription(),hostReviewCreated.getScore());
+        reviewRepository.save(review);
+        return "HostReview Created";
+    }
+
+    public String createCustomerReview(CustomerReviewCreated customerReviewCreated) {
+        Review review = Review.of(customerReviewCreated.getCommentBy(),customerReviewCreated.getCommentTo(),customerReviewCreated.getDescription(),customerReviewCreated.getScore());
+        reviewRepository.save(review);
+        return "HostReview Created";
+    }
 }
--- airbnb/review/src/main/java/com/example/review/controller/ReviewController.java
+++ airbnb/review/src/main/java/com/example/review/controller/ReviewController.java
@@ -1,12 +1,22 @@
 package com.example.review.controller;
 
 import com.example.review.aggregate.ReviewService;
+import com.example.review.model.CustomerReviewCreated;
+import com.example.review.model.HostReviewCreated;
+import com.example.review.model.Review;
+import com.example.review.model.RoomReviewCreated;
+import com.example.review.support.response.AirbnbReviewResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/review")
+@RequestMapping("/api/reviews")
 public class ReviewController {
     private ReviewService reviewService;
 
@@ -14,4 +24,27 @@ public class ReviewController {
     public ReviewController(ReviewService reviewService){
         this.reviewService = reviewService;
     }
+
+    @PostMapping("/room")
+    public AirbnbReviewResponse createRoomReview(@RequestBody RoomReviewCreated roomReviewCreated){
+        // TODO: 예외처리 추가 ( 사용자 있는지 확인 + 방 있는지 확인)
+        String result = reviewService.createRoomReview(roomReviewCreated);
+        return new AirbnbReviewResponse(result);
+    }
+
+    @PostMapping("/host")
+    public AirbnbReviewResponse createHostReview(@RequestBody HostReviewCreated hostReviewCreated){
+        // TODO: 예외처리 추가 ( 사용자 있는지 확인 host & customer )
+        String result = reviewService.createHostReview(hostReviewCreated);
+        return new AirbnbReviewResponse(result);
+    }
+
+    @PostMapping("/customer")
+    public AirbnbReviewResponse createCustomerReview(@RequestBody CustomerReviewCreated customerReviewCreated){
+        // TODO: 예외처리 추가 ( 사용자 있는지 확인 host & customer )
+        String result = reviewService.createCustomerReview(customerReviewCreated);
+        return new AirbnbReviewResponse(result);
+    }
+
+
 }
